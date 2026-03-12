7385f13e786b4dd9730f3dade23410e55378e539
Sebastien_besse <sebastien_besse@yahoo.com>
2026-03-12 11:22:13 +0100

    initial commit

---
+++ Nebulo/Nebulo/NebuloApp.swift
@@ -0,0 +1,17 @@
+//
+//  NebuloApp.swift
+//  Nebulo
+//
+//  Created by apprenant152 on 12/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct NebuloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Nebulo/NebuloTests/NebuloTests.swift
@@ -0,0 +1,17 @@
+//
+//  NebuloTests.swift
+//  NebuloTests
+//
+//  Created by apprenant152 on 12/03/2026.
+//
+
+import Testing
+@testable import Nebulo
+
+struct NebuloTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
