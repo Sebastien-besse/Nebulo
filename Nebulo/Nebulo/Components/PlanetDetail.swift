496cd744839c759b289c94480186b95a32d72a8c
Sebastien_besse <sebastien_besse@yahoo.com>
2026-04-21 15:36:09 +0200

    add fake data

---
+++ Nebulo/Nebulo/Components/PlanetDetail.swift
@@ -0,0 +1,18 @@
+//
+//  PlanetDetail.swift
+//  Nebulo
+//
+//  Created by apprenant152 on 20/03/2026.
+//
+
+import SwiftUI
+
+struct PlanetDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlanetDetail()
+}
